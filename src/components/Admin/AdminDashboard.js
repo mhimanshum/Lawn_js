bd1f50ef435340db1bd7d38dbb8963397ef25806
Prakhar Maurya <000.excel@gmail.com>
2023-05-31 08:32:51 +0530

    category tree parse added

--- src/components/Admin/AdminDashboard.js
+++ src/components/Admin/AdminDashboard.js
@@ -2,60 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { client } from '../../api';
 import Tree from '../Tree/Index';
 
+function assetsChildAdder(assets, c) {
+  return assets.filter((cat0) => c.id === cat0.cid).map((a1) => { return { aid: a1.id, catId: a1.cid, name: a1.name, price: a1.price, description: a1.description } })
+}
+
 function AdminDashboard() {
   const [categories, setCategories] = useState([]);
   const handleDashboard = async () => {
     try {
       const categoryRes = await client.get('/categories');
       const assetRes = await client.get('/assets');
-      // setCategories(res.data.categories);
-      // console.log(res);
-      const data = [];
-      const finalCate = [];
-      console.log(categoryRes.data.categories);
-      categoryRes.data.categories.map((element) => {
-        if (!element.cid) {
-          return data.push(element);
-        }
-      });
-      data.forEach((el) => {
-        el.children = [];
-        categoryRes.data.categories.forEach((item) => {
-          if (el.id === item.cid) {
-            el.children.push(item);
-          }
-          console.log(el);
-        });
-        if (el.children.length === 0 && !el.children.isAssets) {
-          assetRes.data.assets.map((elem) => {
-            if (el.id === elem.cid) {
-              el.children.push(elem);
-            }
-          });
-          el.children.isAssets = true;
-        }
-        el.children.forEach((subItem) => {
-          if (!subItem.children) {
-            subItem.children = [];
-          }
-          categoryRes.data.categories.map((elem) => {
-            if (subItem.id === elem.cid) {
-              subItem.children.push(elem);
-            }
-          });
-          if (subItem.children.length === 0) {
-            assetRes.data.assets.map((elem) => {
-              if (subItem.id === elem.cid) {
-                subItem.children.push(elem);
-              }
-            });
-          }
-        });
 
-        finalCate.push(el);
+      const list = categoryRes.data.categories.filter((c) => c.cid === null)
+
+      list.forEach(c1 => {
+        c1.children = categoryRes.data.categories.filter((cat1) => c1.id === cat1.cid)
+        if (c1.children.length === 0) {
+          c1.children = assetsChildAdder(assetRes.data.assets, c1)
+        }
+
+        c1.children.forEach(c2 => {
+          c2.children = categoryRes.data.categories.filter((cat2) => c2.id === cat2.cid)
+          if (c2.children.length === 0) {
+            c2.children = assetsChildAdder(assetRes.data.assets, c2)
+          }
+          c2.children.forEach(c3 => {
+            c3.children = assetsChildAdder(assetRes.data.assets, c3)
+          });
+        });
       });
-      setCategories(finalCate);
-      console.log(categories);
+
+      setCategories(list)
     } catch (error) {
       console.log(error);
     }
